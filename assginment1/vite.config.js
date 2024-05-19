6ec2dcc7bc026647bd6da2267bc5fd085e2706fb
wilson <caozhaolong@webull.com>
2024-05-19 18:59:36 -0400

    fix: bugs

--- assginment1/vite.config.js
+++ assginment1/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 确保这里是 'build'
+  },
 })
